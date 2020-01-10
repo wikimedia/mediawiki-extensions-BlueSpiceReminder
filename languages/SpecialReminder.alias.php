616d2d2a341e715bcd5e708a1adfc75bfc631479
Umherirrender <umherirrender_de.wp@web.de>
2020-01-10 15:07:00 +0100

    i18n: Restructure alias file
    Alias files are separeted with the language name in all other
    extensions, than it the best to use that format also here
    
    Change-Id: I6e1b35e87d8dd8cee4c05d3227afe97a23222ebc
    (cherry picked from commit c5855c7ad1edcc848f62ddb65634e0643c0b9999)

--- languages/SpecialReminder.alias.php
+++ languages/SpecialReminder.alias.php
@@ -5,18 +5,21 @@
  * Part of BlueSpice MediaWiki
  *
  * @author     Sebastian Ulbricht <sebastian.ulbricht@dragon-network.hk>
- * @version    $Id: SpecialReview.alias.php 8363 2013-01-30 07:57:11Z swidmann $
  * @package    BlueSpice_Extensions
  * @subpackage Review
  * @copyright  Copyright (C) 2016 Hallo Welt! GmbH, All rights reserved.
  * @license    http://www.gnu.org/copyleft/gpl.html GPL-3.0-only
  * @filesource
  */
-$specialPageAliases = [
-	'en' => [
-		'Reminder' => [ 'Reminder' ]
-	],
-	'de' => [
-		'Reminder' => [ 'Erinnerung' ]
-	]
+
+$specialPageAliases = [];
+
+/** English (English) */
+$specialPageAliases['en'] = [
+	'Reminder' => [ 'Reminder' ],
+];
+
+/** German (Deutsch) */
+$specialPageAliases['de'] = [
+	'Reminder' => [ 'Erinnerung' ],
 ];
