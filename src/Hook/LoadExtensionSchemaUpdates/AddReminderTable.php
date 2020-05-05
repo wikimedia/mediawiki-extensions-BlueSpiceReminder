e3b48989d3260d3203d17b58806120c35d4f6cef
Dejan Savuljesku <d.savuljesku@gmail.com>
2020-05-05 11:19:33 +0200

    Fix Sql patch file paths
    [3.1.x]
    
    ERM 19267
    
    Change-Id: I240215f8b97de285362e87f36dc81325131ca8f5

--- src/Hook/LoadExtensionSchemaUpdates/AddReminderTable.php
+++ src/Hook/LoadExtensionSchemaUpdates/AddReminderTable.php
@@ -16,21 +16,21 @@ class AddReminderTable extends \BlueSpice\Hook\LoadExtensionSchemaUpdates {
 		$this->updater->modifyExtensionField(
 			'bs_reminder',
 			'remind_date',
-			"$dir/bs_reminder.patch.modify_remind_date.sql"
+			"$dir/maintenance/db/bs_reminder.patch.modify_remind_date.sql"
 		);
 
 		// BS 2.23.3: prefix columns in reminder
 		$this->updater->modifyExtensionField(
 			'bs_reminder',
 			'id',
-			"$dir/bs_reminder.patch.add_col_prefix.sql"
+			"$dir/maintenance/db/bs_reminder.patch.add_col_prefix.sql"
 		);
 
 		// BS 2.23.3: add comment field
 		$this->updater->addExtensionField(
 			'bs_reminder',
 			'rem_comment',
-			"$dir/bs_reminder.patch.add_comment_col.sql"
+			"$dir/maintenance/db/bs_reminder.patch.add_comment_col.sql"
 		);
 	}
 
