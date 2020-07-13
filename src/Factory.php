38f37d44b2ca537862a400a4c921cc7f35cf624a
pwirth <wirth@hallowelt.biz>
2020-07-13 09:27:26 +0200

    Fix reminder are created and overwritten with expiry id
    This adds a new basic registry for types, reminders for other types can
    be managed. It is currently a bit hacky and should be fixed in next
    minor
    
    ERM:20046
    [3.1.x]
    
    Change-Id: I51cfeb24061f74336653f4361ecd2e493b78d60f

---
+++ src/Factory.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace BlueSpice\Reminder;
+
+use BlueSpice\ExtensionAttributeBasedRegistry;
+use Config;
+
+class Factory {
+
+	/**
+	 *
+	 * @var ExtensionAttributeBasedRegistry
+	 */
+	protected $registry = null;
+
+	/**
+	 *
+	 * @var Config
+	 */
+	protected $config = null;
+
+	/**
+	 *
+	 * @param ExtensionAttributeBasedRegistry $registry
+	 * @param Config $config
+	 */
+	public function __construct( ExtensionAttributeBasedRegistry $registry, Config $config ) {
+		$this->registry = $registry;
+		$this->config = $config;
+	}
+
+	/**
+	 *
+	 * @param string $type
+	 * @return IReminder|null
+	 */
+	public function newFromType( $type ) {
+		if ( empty( $type ) ) {
+			$type = 'page';
+		}
+		$callback = $this->registry->getValue( $type, '' );
+		if ( !is_callable( $callback ) ) {
+			return null;
+		}
+		$instance = call_user_func_array( $callback, [ $type, $this->config ] );
+		return $instance;
+	}
+
+	/**
+	 *
+	 * @param string $type
+	 * @return bool
+	 */
+	public function isRegisteredType( $type ) {
+		if ( empty( $type ) ) {
+			$type = 'page';
+		}
+		return in_array( $type, $this->getRegisteredTypes() );
+	}
+
+	/**
+	 *
+	 * @return string[]
+	 */
+	public function getRegisteredTypes() {
+		return array_merge( $this->registry->getAllKeys(), [ '' ] );
+	}
+}
---
+++ src/Reminder/Page.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace BlueSpice\Reminder\Reminder;
+
+use BlueSpice\Reminder\Reminder;
+
+class Page extends Reminder {
+
+	/**
+	 *
+	 * @return string
+	 */
+	public function getLabelMsgKey() {
+		return 'bs-reminder-type-page';
+	}
+
+}
