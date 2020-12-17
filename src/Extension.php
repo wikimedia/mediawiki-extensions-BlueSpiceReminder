7c07d881371ecc6a7a53defbb221f8b6e91b520b
pwirth <wirth@hallowelt.biz>
2020-12-17 12:23:16 +0100

    Upgrade to BS3 architecture
    ERM:10376
    [3.3]
    
    Change-Id: I0df4399758d2d32fc563f395997d06a05ad29f6d

---
+++ src/Extension.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Reminder Extension for BlueSpice
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * This file is part of BlueSpice MediaWiki
+ * For further information visit https://bluespice.com
+ *
+ *
+ * @author     Sebastian Ulbricht <sebastian.ulbricht@dragon-network.hk>
+ * @author     Tobias Weichart <weichart@hallowelt.com>
+ * @version    4.0
+ * @package    BlueSpice_pro
+ * @subpackage Reminder
+ * @copyright  Copyright (C) 2020 Hallo Welt! GmbH, All rights reserved.
+ * @license    http://www.gnu.org/copyleft/gpl.html GPL-3.0-only
+ * @filesource
+ */
+
+namespace BlueSpice\Reminder;
+
+use BlueSpice\Extension as ExtensionBase;
+
+/**
+ * Main class for Reminder extension
+ * @package BlueSpice_pro
+ * @subpackage Reminder
+ */
+class Extension extends ExtensionBase {
+}
