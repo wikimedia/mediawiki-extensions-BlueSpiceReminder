bcc75b3186539b12519e546652124efc57163714
pwirth <wirth@hallowelt.biz>
2021-01-21 13:24:35 +0100

    Move Reminder manager link to management navigation
    ERM:21178
    [3.2]
    
    Change-Id: Ie6b5e6c89b82e57b404cd490895e94958124514f

---
+++ src/AdminTool.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace BlueSpice\Reminder;
+
+use BlueSpice\IAdminTool;
+use MediaWiki\MediaWikiServices;
+use Message;
+use SpecialPage;
+
+class AdminTool implements IAdminTool {
+
+	/**
+	 *
+	 * @return string
+	 */
+	public function getURL() {
+		$tool = SpecialPage::getTitleFor( 'Reminder' );
+		return $tool->getLocalURL();
+	}
+
+	/**
+	 *
+	 * @return Message
+	 */
+	public function getDescription() {
+		return Message::newFromKey( 'bs-reminder-extension-description' );
+	}
+
+	/**
+	 *
+	 * @return Message
+	 */
+	public function getName() {
+		return Message::newFromKey( 'specialreminder' );
+	}
+
+	/**
+	 *
+	 * @return array
+	 */
+	public function getClasses() {
+		return [
+			'bs-admin-link icon-flag'
+		];
+	}
+
+	/**
+	 *
+	 * @return array
+	 */
+	public function getDataAttributes() {
+		return [];
+	}
+
+	/**
+	 *
+	 * @return array
+	 */
+	public function getPermissions() {
+		$specialReminder = MediaWikiServices::getInstance()
+			->getSpecialPageFactory()
+			->getPage( 'Reminder' );
+		return [
+			$specialReminder->getRestriction()
+		];
+	}
+
+}
