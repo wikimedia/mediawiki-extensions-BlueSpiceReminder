17a70d19b856fd0c288d2d50c41fe8e9c6b11419
d.savuljesku <savuljesku@hallowelt.com>
2021-08-27 10:54:57 +0200

    Use normal BS UserCombo for user selection
    Previously, it used the built-in API to get users, in which search works differently,
    so does not work for this ExtJS component.
    
    Im not sure why it was used, it is possible there was a special reason,
    but i found no issue with just using the normal UserPicker component.
    
    [3.2.x]
    
    ERM 25317
    
    Change-Id: I23965cc56a5e13f5c0e0e0ef791f6c4819e475cf

--- resources/BS.Reminder/PanelDialog.js
+++ resources/BS.Reminder/PanelDialog.js
@@ -2,40 +2,13 @@ Ext.define( "BS.Reminder.PanelDialog", {
 	extend: "BS.Reminder.BaseDialog",
 	makeItems: function () {
 		var items = this.callParent( arguments );
-		this.usersStore = Ext.create( 'Ext.data.Store', {
-			fields: [
-				'userid', 'name'
-			],
-			proxy: {
-				type: 'ajax',
-				url: mw.util.wikiScript( 'api' ),
-				reader: {
-					type: 'json',
-					rootProperty: 'query.allusers',
-					idProperty: 'userid'
-				},
-				extraParams: {
-					format: 'json',
-					action: 'query',
-					list: 'allusers',
-					aulimit: 'max',
-				}
-			},
-			autoLoad: true
-		});
 
 		this.cbTargetPage = Ext.create( 'BS.form.field.TitleCombo', {
 			fieldLabel: mw.message( 'bs-reminder-article-label' ).plain(),
 			labelAlign: 'right'
 		});
-		this.cbxUser = Ext.create( 'Ext.form.field.ComboBox', {
-			fieldLabel: mw.message( 'bs-reminder-user-label' ).plain(),
-			labelAlign: 'right',
-			store: this.usersStore,
-			displayField: 'name',
-			valueField: 'userid',
-			typeAhead: true
-		} );
+
+		this.cbxUser = Ext.create( 'BS.form.UserCombo' );
 
 		items.push( this.cbTargetPage );
 		items.push( this.cbxUser );
