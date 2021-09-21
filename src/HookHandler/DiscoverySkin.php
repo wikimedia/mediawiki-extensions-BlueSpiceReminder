09adbfa41ec710677aefe8e54bb6af95be656cc6
pwirth <wirth@hallowelt.biz>
2021-09-21 12:50:54 +0200

    Move "set reminder" to secondary actions in discovery skin
    ERM:25778
    [4.x]
    
    Change-Id: I35d440c1a2276988c48a6020bf2e16676e1ca654

---
+++ src/HookHandler/DiscoverySkin.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace BlueSpice\Reminder\HookHandler;
+
+use BlueSpice\Discovery\Hook\BlueSpiceDiscoveryTemplateDataProviderAfterInit;
+use BlueSpice\Discovery\ITemplateDataProvider;
+use BlueSpice\Reminder\GlobalActionsManager;
+use MWStake\MediaWiki\Component\CommonUserInterface\Hook\MWStakeCommonUIRegisterSkinSlotComponents;
+
+class DiscoverySkin implements
+	MWStakeCommonUIRegisterSkinSlotComponents,
+	BlueSpiceDiscoveryTemplateDataProviderAfterInit
+{
+
+	/**
+	 * @inheritDoc
+	 */
+	public function onMWStakeCommonUIRegisterSkinSlotComponents( $registry ): void {
+		$registry->register(
+			'GlobalActionsManager',
+			[
+				'special-bluespice-reminder' => [
+					'factory' => function () {
+						return new GlobalActionsManager();
+					}
+				]
+			]
+		);
+	}
+
+	/**
+	 *
+	 * @param ITemplateDataProvider $registry
+	 * @return void
+	 */
+	public function onBlueSpiceDiscoveryTemplateDataProviderAfterInit( $registry ): void {
+		$registry->unregister( 'toolbox', 'ca-reminderCreate' );
+		$registry->register( 'actions_secondary', 'ca-reminderCreate' );
+	}
+}
