5a1d04f18e097d289068de2306afb225348ebe00
miriam schlindwein <schlindwein@hallowelt.com>
2021-10-23 13:22:57 +0200

    Add reminder to personal urls
    ERM26269
    
    [4.x]
    
    Change-Id: If6a40cb3b7f3f562cd5a06dce0041be8babd6a2d

---
+++ src/Hook/PersonalUrls/AddReminderUrl.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace BlueSpice\Reminder\Hook\PersonalUrls;
+
+use BlueSpice\Hook\PersonalUrls;
+
+class AddReminderUrl extends PersonalUrls {
+
+	protected function skipProcessing() {
+		$user = $this->getContext()->getUser();
+		return !$user->isRegistered();
+	}
+
+	protected function doProcess() {
+		$user = $this->getContext()->getUser();
+		$reminder = $this->getServices()->getSpecialPageFactory()->getPage(
+			'Reminder'
+		);
+		$this->personal_urls['my_reminder'] = [
+			'href' => $reminder->getPageTitle()->getLocalURL() . '/'
+			. $user->getName(),
+			'text' => $this->msg( 'bs-reminder-menu_entry-show' )->plain()
+		];
+
+		return true;
+	}
+
+}
