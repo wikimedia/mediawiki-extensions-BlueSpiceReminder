361429581e30ce76b809cd4891813a23ffc702b4
Pavlo Yashchenko <pyashchenko@hallowelt.com>
2022-07-18 14:43:42 +0200

    Load all dependencies of "Set reminder" dialog
    It would crash otherwise, if `OOJSPlus` RL modules are not loaded SetReminder dlg-Window cause an Error
    
    ERM29258
    
    [4.2]
    
    Change-Id: Ied42a117b0f8b82034f7f49f81a0e9ad57886212

--- resources/bluespice.reminder.js
+++ resources/bluespice.reminder.js
@@ -1,8 +1,14 @@
 (function( mw, $, d, bs, undefined ){
 	bs.util.registerNamespace( 'bs.reminder.ui' );
 	bs.util.registerNamespace( 'bs.reminder.ui.mixin' );
-	$( d ).on( 'click', "#ca-reminderCreate, .ca-reminderCreate", function ( e ) {
-		e.preventDefault();
+
+	function _showDialog() {
+		var dfd = $.Deferred();
+		bs.reminder.canEditAll().done( function() {
+				dfd.resolve( getPages( true ) );
+		} ).fail( function() {
+			dfd.resolve( getPages( false ) );
+		} );
 
 		function getPages( canEditAll ) {
 			return [
@@ -20,22 +26,12 @@
 		var dialog = new OOJSPlus.ui.dialog.BookletDialog( {
 			id: 'bs-reminder-dialog-create',
 			pages: function() {
-				var dfd = $.Deferred();
-				mw.loader.using( "ext.bluespice.reminder.dialog.pages", function() {
-					bs.reminder.canEditAll().done( function() {
-						dfd.resolve( getPages( true ) );
-					} ).fail( function() {
-						dfd.resolve( getPages( false ) );
-					} );
-				}, function( e ) {
-					dfd.reject( e );
-				} );
 				return dfd.promise();
 			}
 		} );
 
 		dialog.show();
-	} );
+	};
 
 	bs.reminder.canEditAll = function() {
 		var dfd = $.Deferred();
@@ -51,4 +47,8 @@
 		return dfd.promise();
 	};
 
+	$( d ).on( 'click', "#ca-reminderCreate, .ca-reminderCreate", function ( e ) {
+		e.preventDefault();
+		mw.loader.using( 'ext.bluespice.reminder.dialog.pages' ).done( _showDialog );
+	} );
 })( mediaWiki, jQuery, document, blueSpice );
