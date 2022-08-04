b077d21c01cf383186261de7e20c546e61682bfb
pavloyashchenko <pyashchenko@hallowelt.com>
2022-08-04 18:00:54 +0200

    Create AbstractSchema for sql table
    Create Json file; Generate 3 files(sqlite, postgres, mysql); Change path in addExtensionTable() to generated file.
    
    ERM29227
    
    [4.2.x]
    
    Change-Id: I6363eb9e9bf362b051f3d138db58ac7763d42bfb

--- src/Hook/LoadExtensionSchemaUpdates/AddReminderTable.php
+++ src/Hook/LoadExtensionSchemaUpdates/AddReminderTable.php
@@ -5,57 +5,60 @@ namespace BlueSpice\Reminder\Hook\LoadExtensionSchemaUpdates;
 class AddReminderTable extends \BlueSpice\Hook\LoadExtensionSchemaUpdates {
 
 	protected function doProcess() {
+		$dbType = $this->updater->getDB()->getType();
 		$dir = $this->getExtensionPath();
 
 		$this->updater->addExtensionTable(
 			'bs_reminder',
-			"$dir/maintenance/db/bs_reminder.sql"
+			"$dir/maintenance/db/sql/$dbType/bs_reminder-generated.sql"
 		);
 
-		// BS 2.23.3: Rename remind_date => reminder_date
-		$this->updater->modifyExtensionField(
-			'bs_reminder',
-			'remind_date',
-			"$dir/maintenance/db/bs_reminder.patch.modify_remind_date.sql"
-		);
+		if ( $dbType == 'mysql' ) {
+			// BS 2.23.3: Rename remind_date => reminder_date
+			$this->updater->modifyExtensionField(
+				'bs_reminder',
+				'remind_date',
+				"$dir/maintenance/db/bs_reminder.patch.modify_remind_date.sql"
+			);
 
-		// BS 2.23.3: prefix columns in reminder
-		$this->updater->modifyExtensionField(
-			'bs_reminder',
-			'id',
-			"$dir/maintenance/db/bs_reminder.patch.add_col_prefix.sql"
-		);
+			// BS 2.23.3: prefix columns in reminder
+			$this->updater->modifyExtensionField(
+				'bs_reminder',
+				'id',
+				"$dir/maintenance/db/bs_reminder.patch.add_col_prefix.sql"
+			);
 
-		// BS 2.23.3: add comment field
-		$this->updater->addExtensionField(
-			'bs_reminder',
-			'rem_comment',
-			"$dir/maintenance/db/bs_reminder.patch.add_comment_col.sql"
-		);
+			// BS 2.23.3: add comment field
+			$this->updater->addExtensionField(
+				'bs_reminder',
+				'rem_comment',
+				"$dir/maintenance/db/bs_reminder.patch.add_comment_col.sql"
+			);
 
-		$this->updater->addExtensionField(
-			'bs_reminder',
-			'rem_is_repeating',
-			"$dir/maintenance/db/bs_reminder.patch.add_is_repeating_col.sql"
-		);
+			$this->updater->addExtensionField(
+				'bs_reminder',
+				'rem_is_repeating',
+				"$dir/maintenance/db/bs_reminder.patch.add_is_repeating_col.sql"
+			);
 
-		$this->updater->addExtensionField(
-			'bs_reminder',
-			'rem_repeat_date_end',
-			"$dir/maintenance/db/bs_reminder.patch.add_repeat_date_end_col.sql"
-		);
+			$this->updater->addExtensionField(
+				'bs_reminder',
+				'rem_repeat_date_end',
+				"$dir/maintenance/db/bs_reminder.patch.add_repeat_date_end_col.sql"
+			);
 
-		$this->updater->addExtensionField(
-			'bs_reminder',
-			'rem_repeat_config',
-			"$dir/maintenance/db/bs_reminder.patch.add_repeat_config_col.sql"
-		);
+			$this->updater->addExtensionField(
+				'bs_reminder',
+				'rem_repeat_config',
+				"$dir/maintenance/db/bs_reminder.patch.add_repeat_config_col.sql"
+			);
 
-		$this->updater->addExtensionField(
-			'bs_reminder',
-			'rem_type',
-			"$dir/maintenance/db/bs_reminder.patch.add_type_col.sql"
-		);
+			$this->updater->addExtensionField(
+				'bs_reminder',
+				'rem_type',
+				"$dir/maintenance/db/bs_reminder.patch.add_type_col.sql"
+			);
+		}
 	}
 
 	/**
